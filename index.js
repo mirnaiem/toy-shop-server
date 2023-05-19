49aadacd830035dff1d31f4284256ac9e98791b8
mirnaiem <mirnaiem8teen@gmail.com>
2023-05-19 10:50:09 +0600

    mongodb connect

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const app=express();
 const port=process.env.PORT || 3000;
 
-//n2tFByRQmZthwsTE
+// middleware
 app.use(cors());
 app.use(express.json())
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tefm3zp.mongodb.net/?retryWrites=true&w=majority`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
